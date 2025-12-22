ffe53569a65ef08933e3a763cc93d9bfe486847a
kimbyeong-9 <gkrjawnrkf1@gmail.com>
2025-12-22 16:42:25 +0900

    feat: [김병호] HTML , h1~h5, p, ul, ol HTML 태그 학습 [PMIF-x]

---
+++ KimByeoungHo/html5_css3/html-css-app/src/App.tsx
@@ -0,0 +1,41 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <div>
+        <h1>Hello, World!</h1>
+        <h2>Hello, World!</h2>
+        <h3>Hello, World!</h3>
+        <h4>Hello, World!</h4>
+        <h5>Hello, World!</h5>
+
+        <p>단락 paragraph의 역할입니다.</p>
+
+
+        {/* 리스트(List) */}
+
+        {/* ul= unordered list */}
+        <ul>
+          <li>리스트 1</li>
+          <li>리스트 2</li>
+          <li>리스트 3</li> 
+          <li>리스트 4</li>
+          <li>리스트 5</li>
+        </ul>
+
+         {/* ol= ordered list */}
+        <ol>
+          <li>리스트 1</li>
+          <li>리스트 2</li>
+          <li>리스트 3</li> 
+          <li>리스트 4</li>
+          <li>리스트 5</li>
+        </ol>
+      </div>
+    </>
+  )
+}
+
+export default App
